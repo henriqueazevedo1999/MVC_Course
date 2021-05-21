7980c84c337183299342312e8c4b77107c5ff160
Henrique Feliciano de Azevedo <henriqueazevedo1999@gmail.com>
2021-05-21 01:02:39 -0300

    Add project files.

---
+++ CursoMVC/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Category
+    {
+        [Display(Name = "ID")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="O campo é obrigatório")]
+        [Display(Name = "Descrição")]
+        public string Description { get; set; }
+
+        [Display(Name = "Produtos")]
+        public List<Product> Products { get; set; }
+}
+}
---
+++ CursoMVC/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Product
+    {
+        [Display(Name = "ID")]
+        public int Id { get; set; }
+
+        [Display(Name = "Descrição")]
+        public string Description { get; set; }
+
+        [Display(Name = "Quantidade")]
+        public int Quantity { get; set; }
+
+        [Display(Name = "Categoria")]
+        public int CategoryId { get; set; }
+
+        [Display(Name = "Categoria")]
+        public Category Category { get; set; }
+    }
+}
